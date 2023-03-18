98e0a5b12388b762a2da321789a376592052179f
unknown <ankushdhiman028@gmail.com>
2023-03-18 12:27:05 +0530

    comment

---
+++ js milestone exam project 1/Banking Application1/script.js
@@ -0,0 +1,33 @@
+ // let btn = document.getElementById("accountOpenBtn")
+// btn.addEventListener("click" , myFun)
+// function myFun(){
+//     let inputvalue = document.getElementById("name");
+//     let x = inputvalue.value; 
+//     console.log(x)
+//     // let userName = document.getElementById("username");
+//     // userName.textContent= x;
+//     if( inputvalue == " "){
+//         alert("enter value")
+//     }
+//     else{
+//         window.location.href = "Userprofile.html"
+//     }
+// }   
+let btn = document.getElementById("accountOpenBtn")
+btn.addEventListener("click" , myFun)
+function myFun(){
+    let inputvalue = document.getElementById("name");
+    let x = inputvalue.value; 
+    
+    let userName = x;
+    let accountnum ="Account No :" + "30420100000" + Math.floor(Math.random()*(2000-1000)+1000);
+   
+    if( x == ""){
+        alert("enter value")
+    }
+    else{
+        localStorage.setItem("uname",userName);
+        localStorage.setItem("acn",accountnum);
+        window.location.href = "Userprofile.html"
+    }
+}
